ed91f8f4d6c4fef5975d76ff30dada6a342758de
Thomas Kee <xsited@yahoo.com>
2014-10-08 12:53:12 -0700

    Header with EPL copyright notice for driver
    Change-Id: I60f8599372c84155e6867e374a2a55bbb0fb1188
    Signed-off-by: Thomas Kee <xsited@yahoo.com>

--- protocol_plugins.packetcable/src/main/java/org/pcmm/rcd/impl/CMTS.java
+++ protocol_plugins.packetcable/src/main/java/org/pcmm/rcd/impl/CMTS.java
@@ -1,5 +1,11 @@
 /**
- @header@
+ 
+ * Copyright (c) 2014 CableLabs.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+
  */
 package org.pcmm.rcd.impl;
 
