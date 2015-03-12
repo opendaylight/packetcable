355bca473032caf4467c7fa0454a7d5a84b5574e
Steven Pisarski <s.pisarski@cablelabs.com>
2015-03-12 09:09:18 -0600

    Changed System.err calls to use logger instead.
    Change-Id: I0b05fbee1e9e74f6d7821bfefdff72405cf1a6b5
    Signed-off-by: Steven Pisarski <s.pisarski@cablelabs.com>

--- packetcable-driver/src/main/java/org/umu/cops/common/COPSDebug.java
+++ packetcable-driver/src/main/java/org/umu/cops/common/COPSDebug.java
@@ -5,7 +5,8 @@
  */
 package org.umu.cops.common;
 
-import java.io.PrintStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class to print debug and error messages.
@@ -20,7 +21,7 @@ public class COPSDebug {
     static public final String ERROR_SOCKET = "Error socket";
     static public final String ERROR_NOSUPPORTED = "Object not supported";
 
-    static public PrintStream _err = System.err;
+    private static final Logger logger = LoggerFactory.getLogger(COPSDebug.class);
 
     /** Prints an error message.
         *
@@ -28,9 +29,9 @@ public class COPSDebug {
      * @param          str                Error message
      *
        */
+    @Deprecated
     public static void err (String cname, String str) {
-        if (_err != null)
-            _err.println(cname + ":" + str);
+        logger.info(cname + ":" + str);
     }
 
     /** Prints an error message.
@@ -40,11 +41,9 @@ public class COPSDebug {
      * @param           e                  Exception
      *
        */
+    @Deprecated
     public static void err (String cname, String str, Exception e) {
-        if (_err != null) {
-            _err.println(cname + ":" + str);
-            _err.println(" -Reason: " + e.getMessage());
-        }
+        logger.info(cname + ":" + str, e);
     }
 
     /** Prints an error message.
@@ -55,12 +54,9 @@ public class COPSDebug {
      * @param           e                  Exception
      *
        */
+    @Deprecated
     public static void err (String cname, String str, String extra, Exception e) {
-        if (_err != null) {
-            _err.println(cname + ":" + str);
-            _err.println(" -Info: " + extra);
-            _err.println(" -Reason: " + e.getMessage());
-        }
+        logger.info(cname + ":" + str + " -Info: " + extra, e);
     }
 
     /** Prints an error message.
@@ -70,11 +66,9 @@ public class COPSDebug {
      * @param          extra           Information
      *
        */
+    @Deprecated
     public static void err (String cname, String str, String extra) {
-        if (_err != null) {
-            _err.println(cname + ":" + str);
-            _err.println(" -Info: " + extra);
-        }
+        logger.info(cname + ":" + str + " -Info: " + extra);
     }
 
 }
