ba52ac5dcc70b7b7eb948155c1528c3d3e825978
Steven Pisarski <s.pisarski@cablelabs.com>
2015-05-06 13:34:48 -0600

    Refactor shared attributes and methods to the PEP agent super COPSPepAgent.
    Change-Id: Ibadb787afc54549bc46fc2efcab2f5f0eb9c223e
    Signed-off-by: Steven Pisarski <s.pisarski@cablelabs.com>

--- packetcable-driver/src/main/java/org/umu/cops/ospep/COPSPepOSConnection.java
+++ packetcable-driver/src/main/java/org/umu/cops/ospep/COPSPepOSConnection.java
@@ -1,10 +1,12 @@
 package org.umu.cops.ospep;
 
 import org.umu.cops.prpep.COPSPepConnection;
-import org.umu.cops.stack.*;
+import org.umu.cops.stack.COPSClientSI;
+import org.umu.cops.stack.COPSDecisionMsg;
+import org.umu.cops.stack.COPSException;
+import org.umu.cops.stack.COPSHandle;
 
 import java.net.Socket;
-import java.util.Hashtable;
 import java.util.List;
 
 /**
@@ -22,15 +24,6 @@ public class COPSPepOSConnection extends COPSPepConnection {
         super(clientType, sock);
     }
 
-    /**
-     * Gets all request state managers
-     * @return  A <tt>Hashatable</tt> holding all request state managers
-     * TODO - change the return to Map
-     */
-    protected Hashtable getReqStateMans() {
-        return new Hashtable(_managerMap);
-    }
-
     @Override
     protected void handleDecisionMsg(final COPSDecisionMsg dMsg) throws COPSException {
         final COPSPepOSReqStateMan manager = (COPSPepOSReqStateMan)_managerMap.get(dMsg.getClientHandle());
@@ -39,18 +32,17 @@ public class COPSPepOSConnection extends COPSPepConnection {
 
     /**
      * Adds a new request state
-     * @param clientHandle  Client's handle
+     * @param handle        Client's handle
      * @param process       Policy data processing object
      * @param clientSIs     Client data from the outsourcing event
      * @return              The newly created request state manager
      * @throws COPSException
      */
-    protected COPSPepOSReqStateMan addRequestState(final String clientHandle, final COPSPepOSDataProcess process,
+    protected COPSPepOSReqStateMan addRequestState(final COPSHandle handle, final COPSPepOSDataProcess process,
                                                    final List<COPSClientSI> clientSIs) throws COPSException {
-        final COPSHandle handle = new COPSHandle(new COPSData(clientHandle));
         final COPSPepOSReqStateMan manager = new COPSPepOSReqStateMan(_clientType, handle, process, clientSIs);
         if (_managerMap.get(handle) != null)
-            throw new COPSPepException("Duplicate Handle, rejecting " + clientHandle);
+            throw new COPSPepException("Duplicate Handle, rejecting " + handle.getId().str());
         _managerMap.put(handle, manager);
         manager.initRequestState(_sock);
         return manager;
