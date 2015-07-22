ac59730807097a86c034ebcef10f7a1cea0d0d21
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-07-22 14:07:41 -0400

    DO NOT MERGE - Refactor pom files to reflects right parents.
    Also rename the config file to default-config
    Change the broker to be binding-osgi-borker
    
    This patch doesn't fix the following issue: https://gist.github.com/adetalhouet/9ef2ae2f57533ded5cd5
    
    Change-Id: I8364ec693344c1550225e4f44a77dfd6a68deecb
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- packetcable-policy-server/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/packetcable/packetcable/policy/server/impl/rev140131/PacketcableProviderModule.java
+++ packetcable-policy-server/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/packetcable/packetcable/policy/server/impl/rev140131/PacketcableProviderModule.java
@@ -1,10 +1,6 @@
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.packetcable.packetcable.policy.server.impl.rev140131;
 
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.packetcable.provider.PacketcableProvider;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,37 +23,9 @@ public class PacketcableProviderModule extends org.opendaylight.yang.gen.v1.urn.
     @Override
     public java.lang.AutoCloseable createInstance() {
         logger.info("Creating PacketcableProvider instance");
-
-        final DataBroker dataBrokerService = getDataBrokerDependency();
-        final PacketcableProvider provider = new PacketcableProvider(dataBrokerService);
-
-        final ListenerRegistration<DataChangeListener> ccapDataChangeListenerRegistration =
-                dataBrokerService.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
-                        PacketcableProvider.ccapIID, provider, DataBroker.DataChangeScope.SUBTREE );
-
-        final ListenerRegistration<DataChangeListener> qosDataChangeListenerRegistration =
-                dataBrokerService.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
-                        PacketcableProvider.qosIID, provider, DataBroker.DataChangeScope.SUBTREE );
-
-        logger.info("PacketCableProvider Registered with DataBroker");
-
-        AutoCloseable close = new AutoCloseable() {
-
-            @Override
-            public void close() throws Exception {
-                if (ccapDataChangeListenerRegistration != null) {
-                    ccapDataChangeListenerRegistration.close();
-                }
-                if (qosDataChangeListenerRegistration != null) {
-                    qosDataChangeListenerRegistration.close();
-                }
-                if (provider != null) {
-                    provider.close();
-                }
-            }
-        };
-
-        return close;
+        final PacketcableProvider provider = new PacketcableProvider();
+        getDataBrokerDependency().registerProvider(provider);
+        return provider;
     }
 
 }
