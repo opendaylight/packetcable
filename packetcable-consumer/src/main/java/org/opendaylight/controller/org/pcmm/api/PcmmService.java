381fa8b62af40db9e46b58417c24923a77b0f8a9
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-07-22 17:33:15 -0400

    Remove deprecated modules.
    Thoses modules were deprecated in Lithium, and now obsolete for Berryllium.
    
    Change-Id: I938f337c06381bce10fc30574aabb569b975a3e2
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- packetcable-consumer/src/main/java/org/opendaylight/controller/org/pcmm/api/PcmmService.java
+++
@@ -1,9 +0,0 @@
-package org.opendaylight.controller.org.pcmm.api;
-
-import org.opendaylight.controller.config.yang.config.pcmm_service.impl.PcmmServiceRuntimeMXBean;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.node.cmts.broker.rev140909.PacketcableBrokerListener;
-
-
-public interface PcmmService extends PcmmServiceRuntimeMXBean, PacketcableBrokerListener {
-
-}
