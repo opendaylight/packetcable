dd443a5b706a313f1deebed91eb45e04d92de5e9
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-16 10:38:15 -0500

    Add missing license headers to packetcable-driver scd
    Change-Id: Ib6936f66fe98df759f854be8540d352d4391c7aa
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- packetcable-driver/src/main/java/org/pcmm/scd/IApplicationServer.java
+++ packetcable-driver/src/main/java/org/pcmm/scd/IApplicationServer.java
@@ -1,5 +1,9 @@
-/**
- @header@
+/*
+ * Copyright (c) 2014 Cable Television Laboratories, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
 package org.pcmm.scd;
@@ -24,14 +28,14 @@ package org.pcmm.scd;
  * may deny the request based upon additional Service Control Domain policies or
  * may pass the request on to the Policy Server.
  * </p>
- * 
- * 
+ *
+ *
  */
 public interface IApplicationServer {
 
 	/**
 	 * sets the Application Server's id
-	 * 
+	 *
 	 * @param id
 	 *            : the id of the AS
 	 */
@@ -39,7 +43,7 @@ public interface IApplicationServer {
 
 	/**
 	 * gets the AS id
-	 * 
+	 *
 	 * @return AS id
 	 */
 	String getASId();
--- packetcable-driver/src/main/java/org/pcmm/scd/package-info.java
+++ packetcable-driver/src/main/java/org/pcmm/scd/package-info.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2014 Cable Television Laboratories, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 /**
  * <p>
  * The Service Control Domain (SCD) is defined as a logical grouping of elements that offer applications and content to service subscribers.
