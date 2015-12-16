cfdb9298bee0dcb09e1f837be9baa13c486922a8
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-16 10:25:37 -0500

    Add missing license headers to packetcable-driver gates
    Change-Id: I694943fd0e25e75698d9bbe8615ffc28dfb7655b
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- packetcable-driver/src/main/java/org/pcmm/gates/impl/IPv6Classifier.java
+++ packetcable-driver/src/main/java/org/pcmm/gates/impl/IPv6Classifier.java
@@ -1,12 +1,11 @@
-/**
-
- * Copyright (c) 2014 CableLabs.  All rights reserved.
+/*
+ * Copyright (c) 2014 Cable Television Laboratories, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
-
  */
+
 package org.pcmm.gates.impl;
 
 import com.google.common.primitives.Bytes;
