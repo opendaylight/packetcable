a6d6c62a2c36d7f2dde2075f8755fa6882fabd2a
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-16 10:34:49 -0500

    Add missing license headers to packetcable-driver objects
    Change-Id: I7c3e2ceed3b8dafcdc173556ec688cdc6d0ee63e
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- packetcable-driver/src/main/java/org/pcmm/objects/PCMMResource.java
+++ packetcable-driver/src/main/java/org/pcmm/objects/PCMMResource.java
@@ -1,13 +1,18 @@
-/**
- @header@
+/*
+ * Copyright (c) 2014 Cable Television Laboratories, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.pcmm.objects;
 
 /**
  *
  * This class is a holder for PCMM objects. This class is intended to be
  * sub-classed
- 
+
  *
  */
 public class PCMMResource {
