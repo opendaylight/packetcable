993e392f055aac9b9c758f5349707860e37c2c3c
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-16 10:42:21 -0500

    Add missing license headers to packetcable-driver pcmm
    Change-Id: I220c6d285d10e03c05b62234e18b7a0ae2d6e1e8
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- packetcable-driver/src/main/java/org/pcmm/PCMMGlobalConfig.java
+++ packetcable-driver/src/main/java/org/pcmm/PCMMGlobalConfig.java
@@ -1,5 +1,9 @@
-/**
- @header@
+/*
+ * Copyright (c) 2014, 2015 Cable Television Laboratories, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
 package org.pcmm;
@@ -71,8 +75,8 @@ public class PCMMGlobalConfig {
      public static String SubscriberID = "10.32.104.2";
      public static String srcIP = "10.32.154.2";
  */
- 
-/*  LAB Bench Layout 
+
+/*  LAB Bench Layout
      public static String DefaultCMTS = "10.32.15.3";
      public static String SubscriberID = "10.32.115.143";
      public static String dstIP = "10.32.0.234";
--- packetcable-driver/src/main/java/org/pcmm/PCMMProperties.java
+++ packetcable-driver/src/main/java/org/pcmm/PCMMProperties.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2014, 2015 Cable Television Laboratories, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.pcmm;
 
 import java.io.IOException;
@@ -9,7 +17,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Loads the PCMM Properties file.
- * 
+ *
  */
 public class PCMMProperties implements PCMMConstants {
 
